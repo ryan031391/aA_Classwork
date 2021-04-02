4ac605e8e3b5b2c75de35149737b3017e3de6836
guocong <ryp0313@gmail.com>
2021-04-02 13:43:15 -0700

    Finish PolyTree

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -12,12 +12,24 @@ class PolyTreeNode
             @parent = parent_node
             parent_node.children << self
         elsif parent_node == nil
+            @parent.children.delete(self)
             @parent = nil
         else 
             @parent.children.delete(self)
             @parent = parent_node
             parent_node.children << self
         end
-        
     end
+
+    def add_child(node)
+        node.parent = self
+    end
+
+    def remove_child(node)
+        raise "error" if !@children.include?(node)
+        node.parent = nil
+    end
+
 end
+
+def
