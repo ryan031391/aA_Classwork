0f3fb6d79496081d0a324e51b47e21c4ea767cdf
guocong <ryp0313@gmail.com>
2021-04-05 11:41:53 -0700

    Add new_move_position

--- W4D1/Knight/knight.rb
+++ W4D1/Knight/knight.rb
@@ -1,9 +1,42 @@
+require 'tree.rb'
+
 class KnightPathFinder
-    
-    def initialize(start_pos)
-        @start_pos = start_pos
+
+    def self.root_node(pos)
+        PolyTreeNode.new(pos)
+    end
+
+    def self.valid_moves(pos)
+        row, col = pos
+        (0..7).to_a.include?(row) && (0..7).to_a.include?(col) && !@consider_positon.inclulde?(pos)
     end
     
+    def initialize(start_pos)
+        @start_pos = KnightPathFinder.root_node(start_pos)
+        @consider_positon = [start_pos]
+        # @current_position = consider_positon[-1]
+    end
+
+    def find_path
+
+    end
+
+
     
+    def new_move_position(pos)
+        arr = []
+        row = pos[0]
+        col = pos[1]
+        arr << [row-2,col+1] if KnightPathFinder.valid_moves([row-2,col+1])
+        arr << [row+2,col+1] if KnightPathFinder.valid_moves([row+2,col+1])
+        arr << [row-2,col-1] if KnightPathFinder.valid_moves([row-2,col-1])
+        arr << [row+2,col-1] if KnightPathFinder.valid_moves([row+2,col-1])
+        arr << [row-1,col+2] if KnightPathFinder.valid_moves([row-1,col+2])
+        arr << [row-1,col-2] if KnightPathFinder.valid_moves([row-1,col-2])
+        arr << [row+1,col+2] if KnightPathFinder.valid_moves([row+1,col+2])
+        arr << [row+1,col-2] if KnightPathFinder.valid_moves([row+1,col-2])
+        @consider_positon += arr
+        return arr
+    end
 
 end
--- W4D1/Knight/tree.rb
+++ W4D1/Knight/tree.rb
@@ -2,39 +2,39 @@ class PolyTreeNode
     attr_reader :parent, :children, :value
 
     def initialize(new_node)
-        @grid 
+        # @grid 
         @parent = nil 
         @children = []
         @value = new_node
-        @passed = true
+        # @passed = true
         @child = nil
     end
 
-    def board
-        @grid ||= Array.new(8) {Array.new}
-    end
+    # def board
+    #     @grid ||= Array.new(8) {Array.new}
+    # end
 
-    def parent=(parent_node)
+    def parent
         child.parent = self
     end
-    def valid_child?(arr)
-        row, col = arr
-        (0..7).to_a.include?(row) && (0..7).to_a.include?(col)
-    end
 
-    def find_children
-        row = self.value[0]
-        col = self.value[1]
-        children << [row-2,col+1] if valid_child([row-2,col+1])
-        children << [row+2,col+1] if valid_child([row+2,col+1])
-        children << [row-2,col-1] if valid_child([row-2,col-1])
-        children << [row+2,col-1] if valid_child([row+2,col-1])
-        children << [row-1,col+2] if valid_child([row-1,col+2])
-        children << [row-1,col-2] if valid_child([row-1,col-2])
-        children << [row+1,col+2] if valid_child([row+1,col+2])
-        children << [row+1,col-2] if valid_child([row+1,col-2])
-        
-    end
+    # def valid_child?(arr)
+    #     row, col = arr
+    #     (0..7).to_a.include?(row) && (0..7).to_a.include?(col)
+    # end
+
+    # def find_children
+    #     row = self.value[0]
+    #     col = self.value[1]
+    #     children << [row-2,col+1] if valid_child([row-2,col+1])
+    #     children << [row+2,col+1] if valid_child([row+2,col+1])
+    #     children << [row-2,col-1] if valid_child([row-2,col-1])
+    #     children << [row+2,col-1] if valid_child([row+2,col-1])
+    #     children << [row-1,col+2] if valid_child([row-1,col+2])
+    #     children << [row-1,col-2] if valid_child([row-1,col-2])
+    #     children << [row+1,col+2] if valid_child([row+1,col+2])
+    #     children << [row+1,col-2] if valid_child([row+1,col-2])
+    # end
 
     def next_step
         child = PolyTreeNode.new(children.sample)
