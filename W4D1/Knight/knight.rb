dd1334097176329ca7a4acb811c7cf03ae0ee81a
ziosanfer <ziosanfer@gmail.com>
2021-04-05 16:16:52 -0700

    finished by adding find PATH AND TRACE BACK PATH

--- W4D1/Knight/knight.rb
+++ W4D1/Knight/knight.rb
@@ -1,5 +1,5 @@
-require 'tree.rb'
-
+require_relative 'tree.rb'
+require "byebug"
 class KnightPathFinder
 
     def self.root_node(pos)
@@ -8,17 +8,17 @@ class KnightPathFinder
 
     def self.valid_moves(pos)
         row, col = pos
-        (0..7).to_a.include?(row) && (0..7).to_a.include?(col) && !@consider_positon.inclulde?(pos)
+        (0..7).to_a.include?(row) && (0..7).to_a.include?(col)
     end
-    
+    attr_reader :tree, :consider_position, :start_pos
     def initialize(start_pos)
         @start_pos = KnightPathFinder.root_node(start_pos)
-        @consider_positon = [start_pos]
-        # @current_position = consider_positon[-1]
+        @consider_position = [@start_pos.value]
+        @tree = build_move_tree(@start_pos)
     end
 
-    def build_move_tree
-        path_tree = [@start_pos]
+    def build_move_tree(orig_node)
+        path_tree = [orig_node]
         until path_tree.empty? 
             curr_node = path_tree.shift
             new_move_position(curr_node.value).each do |node|
@@ -28,30 +28,19 @@ class KnightPathFinder
         end
     end
 
-    def make_node(pos, prev_pos = nil)
-        node = PolyTreeNode.new(pos, prev_pos)
-        node.children += new_move_position(pos)
+    def find_path(end_pos)
+        end_node = @start_pos.bfs(end_pos) 
+        trace_back_path(end_node)
     end
 
-
-    # def bfs(target)
-    #     arr = [self]
-    #     until arr.empty?
-    #         if arr[0].value != target
-    #             if !arr[0].children.empty?
-    #                 arr += arr[0].children
-    #                 arr.shift
-    #             else
-    #                 arr.shift
-    #             end
-    #         else
-    #             return arr[0]
-    #         end
-    #     end
-    # end
-
-    def find_path()
-
+    def trace_back_path(node)
+        way = [node.value]
+        curr_node = node
+        until way[0] == @start_pos.value
+            way.unshift(curr_node.parent.value)
+            curr_node = curr_node.parent
+        end
+        way
     end
 
 
@@ -60,18 +49,22 @@ class KnightPathFinder
         arr = []
         row = pos[0]
         col = pos[1]
-        arr << PolyTreeNode.new([row-2,col+1], pos) if KnightPathFinder.valid_moves([row-2,col+1])
-        arr << PolyTreeNode.new([row+2,col+1], pos) if KnightPathFinder.valid_moves([row+2,col+1])
-        arr << PolyTreeNode.new([row-2,col-1], pos) if KnightPathFinder.valid_moves([row-2,col-1])
-        arr << PolyTreeNode.new([row+2,col-1], pos) if KnightPathFinder.valid_moves([row+2,col-1])
-        arr << PolyTreeNode.new([row-1,col+2], pos) if KnightPathFinder.valid_moves([row-1,col+2])
-        arr << PolyTreeNode.new([row-1,col-2], pos) if KnightPathFinder.valid_moves([row-1,col-2])
-        arr << PolyTreeNode.new([row+1,col+2], pos) if KnightPathFinder.valid_moves([row+1,col+2])
-        arr << PolyTreeNode.new([row+1,col-2], pos) if KnightPathFinder.valid_moves([row+1,col-2])
-        arr.each {|ele| @consider_positon << ele.value}
+        arr << PolyTreeNode.new([row-2,col+1]) if KnightPathFinder.valid_moves([row-2,col+1]) && !@consider_position.include?([row-2,col+1])
+        arr << PolyTreeNode.new([row+2,col+1]) if KnightPathFinder.valid_moves([row+2,col+1]) && !@consider_position.include?([row+2,col+1])
+        arr << PolyTreeNode.new([row-2,col-1]) if KnightPathFinder.valid_moves([row-2,col-1]) && !@consider_position.include?([row-2,col-1])
+        arr << PolyTreeNode.new([row+2,col-1]) if KnightPathFinder.valid_moves([row+2,col-1]) && !@consider_position.include?([row+2,col-1])
+        arr << PolyTreeNode.new([row-1,col+2]) if KnightPathFinder.valid_moves([row-1,col+2]) && !@consider_position.include?([row-1,col+2])
+        arr << PolyTreeNode.new([row-1,col-2]) if KnightPathFinder.valid_moves([row-1,col-2]) && !@consider_position.include?([row-1,col-2])
+        arr << PolyTreeNode.new([row+1,col+2]) if KnightPathFinder.valid_moves([row+1,col+2]) && !@consider_position.include?([row+1,col+2])
+        arr << PolyTreeNode.new([row+1,col-2]) if KnightPathFinder.valid_moves([row+1,col-2]) && !@consider_position.include?([row+1,col-2])
+        arr.each {|ele| @consider_position << ele.value}
         return arr
     end
 
 end
 
 
+
+kpf = KnightPathFinder.new([0, 0])
+p kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+p kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
--- W4D1/Knight/tree.rb
+++ W4D1/Knight/tree.rb
@@ -36,28 +36,34 @@ class PolyTreeNode
         node.parent = self
     end
 
-    def parent
-        child.parent = self
+    def parent=(parent_node)
+        if @parent == nil
+            @parent = parent_node
+            parent_node.children << self
+        elsif parent_node == nil
+            @parent.children.delete(self)
+            @parent = nil
+        else 
+            @parent.children.delete(self)
+            @parent = parent_node
+            parent_node.children << self
+        end
     end
 
-    # def valid_child?(arr)
-    #     row, col = arr
-    #     (0..7).to_a.include?(row) && (0..7).to_a.include?(col)
-    # end
-
-    # def find_children
-    #     row = self.value[0]
-    #     col = self.value[1]
-    #     children << [row-2,col+1] if valid_child([row-2,col+1])
-    #     children << [row+2,col+1] if valid_child([row+2,col+1])
-    #     children << [row-2,col-1] if valid_child([row-2,col-1])
-    #     children << [row+2,col-1] if valid_child([row+2,col-1])
-    #     children << [row-1,col+2] if valid_child([row-1,col+2])
-    #     children << [row-1,col-2] if valid_child([row-1,col-2])
-    #     children << [row+1,col+2] if valid_child([row+1,col+2])
-    #     children << [row+1,col-2] if valid_child([row+1,col-2])
-    # end
-
-
+    def bfs(target)
+        arr = [self]
+        until arr.empty?
+            if arr[0].value != target
+                if !arr[0].children.empty?
+                    arr += arr[0].children
+                    arr.shift
+                else
+                    arr.shift
+                end
+            else
+                return arr[0]
+            end
+        end
+    end
 
 end
