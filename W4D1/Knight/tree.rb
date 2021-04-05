048c90eee26bef9d6037dae56364367afc533524
ziosanfer <ziosanfer@gmail.com>
2021-04-05 11:17:17 -0700

    Added find children method && next_step

--- W4D1/Knight/tree.rb
+++ W4D1/Knight/tree.rb
@@ -7,34 +7,41 @@ class PolyTreeNode
         @children = []
         @value = new_node
         @passed = true
+        @child = nil
     end
 
     def board
         @grid ||= Array.new(8) {Array.new}
     end
 
-    # def parent=(parent_node)
-    #     if @parent == nil
-    #         @parent = parent_node
-    #         parent_node.children << self
-    #     elsif parent_node == nil
-    #         @parent.children.delete(self)
-    #         @parent = nil
-    #     else 
-    #         @parent.children.delete(self)
-    #         @parent = parent_node
-    #         parent_node.children << self
-    #     end
-    # end
-    def valid_child?
-
+    def parent=(parent_node)
+        child.parent = self
+    end
+    def valid_child?(arr)
+        row, col = arr
+        (0..7).to_a.include?(row) && (0..7).to_a.include?(col)
     end
 
     def find_children
         row = self.value[0]
         col = self.value[1]
-        children << [row-2,col+1]
+        children << [row-2,col+1] if valid_child([row-2,col+1])
+        children << [row+2,col+1] if valid_child([row+2,col+1])
+        children << [row-2,col-1] if valid_child([row-2,col-1])
+        children << [row+2,col-1] if valid_child([row+2,col-1])
+        children << [row-1,col+2] if valid_child([row-1,col+2])
+        children << [row-1,col-2] if valid_child([row-1,col-2])
+        children << [row+1,col+2] if valid_child([row+1,col+2])
+        children << [row+1,col-2] if valid_child([row+1,col-2])
         
     end
 
+    def next_step
+        child = PolyTreeNode.new(children.sample)
+    end
+
+
 end
+
+bfs dfs
+    
