5705c01783f9db1436064e5e06663fbd7cd96234
Kien Nguyen <nguyen_kien@live.com>
2021-04-08 16:23:45 -0500

    finished two sums

--- W4D3/lib/practice.rb
+++ W4D3/lib/practice.rb
@@ -1,1 +1,23 @@
 # require 'practice_spec.rb'
+class Array
+
+    def remove_dups
+        arr = []
+        self.each do |el|
+            arr << el if !arr.include?(el)
+        end
+        arr
+    end
+
+    def two_sum
+        arr = []
+
+        self.each_with_index do |el, i|
+            (i + 1...self.length).each do |j|
+                arr << [i, j] if el + self[j] == 0
+            end
+        end
+        arr
+    end
+
+end
--- W4D3/spec/practice_spec.rb
+++ W4D3/spec/practice_spec.rb
@@ -1,8 +1,14 @@
 require 'practice'
 require 'rspec'
 
-describe '#remove_dups'
+describe '#remove_dups' do
     it 'method should remove duplicate from the array.' do 
-        expect ([1, 2, 1, 3, 3].uniq).to eq([1, 2, 3])
+        expect([1, 2, 1, 3, 3].remove_dups ).to eq([1, 2, 3])
+    end
+end
+
+describe '#two_sum' do 
+    it 'finds all pairs of positions where the elements at those positions sum to zero' do
+        expect([-1, 0, 2, -2, 1].two_sum).to eq([[0,4], [2, 3]])
     end
 end
