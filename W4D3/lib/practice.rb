a3b69ea5a5f32609240cd869d833af2934af3408
guocong <ryp0313@gmail.com>
2021-04-08 15:00:21 -0700

    Finish transpose

--- W4D3/lib/practice.rb
+++ W4D3/lib/practice.rb
@@ -20,4 +20,36 @@ class Array
         arr
     end
 
+    def my_transpose
+        arr = Array.new(self.length) {Array.new} 
+        self.each_with_index do |el, i|
+            self.each_with_index do |el, j|
+                arr[i] << self[j][i]
+            end
+        end
+        arr 
+    end
+    
+    def stock_picker
+        # min = self[0]
+        # max = self[0]
+        # self.each do |el|
+        #     min = el if min > el
+        #     max = el if max < el
+        # end
+        # [self.index(min), self.index(max)]
+        diff = 0
+        arr = []
+        self.each_with_index do |price, i|
+            (i+1...self.length).each do |j|
+                curr_diff = self[j]- price
+                if diff < curr_diff
+                    diff = curr_diff
+                    arr = [i,j]
+                end
+            end
+        end
+        arr
+    end
+
 end
--- W4D3/spec/practice_spec.rb
+++ W4D3/spec/practice_spec.rb
@@ -12,3 +12,25 @@ describe '#two_sum' do
         expect([-1, 0, 2, -2, 1].two_sum).to eq([[0,4], [2, 3]])
     end
 end
+
+describe '#my_transpose' do 
+    it 'should transpose a matrix' do
+        expect( [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+        ].my_transpose
+        ).to eq([
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]
+          ])
+    end
+end
+
+describe '#stock_picker' do
+    it 'outputs the pair of days which the first day has the lowest price and the second day has the highest pirce' do
+        expect([2,5,7,4,8,5,7,9,1,6].stock_picker).to eq([0,7])
+    end
+end
+
