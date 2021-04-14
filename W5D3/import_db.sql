71731fc8b6983e6516b462ac92e8b44579d15f15
Josiah Leon <sjosiahleon@gmail.com>
2021-04-14 14:11:16 -0700

    Created Import_db

--- W5D3/import_db.sql
+++ W5D3/import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE
 users(
     id INTEGER PRIMARY KEY, 
@@ -21,6 +23,35 @@ questions_follows(
     user_id INTEGER NOT NULL, 
     question_id INTEGER NOT NULL,
 
-    FOREIGN KEY(user_id) REFERENCES users(id)
+    FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(question_id) REFERENCES questions(id)
 );
+
+CREATE TABLE
+    replies(
+        id INTEGER PRIMARY KEY,
+        question_id INTEGER NOT NULL,
+        parent_id INTEGER,
+        user_id INTEGER NOT NULL,
+        body VARCHAR(100),
+
+
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (question_id) REFERENCES questions(id),
+        FOREIGN KEY (parent_id) REFERENCES replies(id)
+    );
+
+
+CREATE TABLE
+    question_likes(
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        question_id INTEGER NOT NULL,
+
+        FOREIGN KEY (user_id) REFERENCES users(id)
+        FOREIGN KEY (question_id) REFERENCES questions(id)
+    );
+
+
+INSERT INTO
+    users (fname, lname)
